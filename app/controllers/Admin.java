4caf59f6d8252b6828bddc3d4dbe245805f32437
Nicolas Schejtman <nschejtman93@gmail.com>
2015-10-13 14:20:23 +0200

    Added basic nav

---
+++ app/controllers/Admin.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.admin.*;
+
+public class Admin extends Controller {
+
+    public Result index() {
+        return ok(index.render());
+    }
+}

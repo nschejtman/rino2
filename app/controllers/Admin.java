8902c791b2718d13f6c24350c38f266bf34e2beb
Nicolas Schejtman <nschejtman93@gmail.com>
2015-11-24 18:51:58 +0100

    Started angular app

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.tournament.Category;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -9,23 +11,27 @@ public class Admin extends Controller {
         return ok(views.html.admin.index.render());
     }
 
-    public Result categories(){
-        return ok(views.html.admin.categories.render());
-    }
+//    public Result categories(){
+//        return ok(views.html.admin.categories.render());
+//    }
 
-    public Result tournaments(){
-        return ok(views.html.admin.tournaments.render());
-    }
-
-    public Result teams(){
-        return ok(views.html.admin.teams.render());
-    }
-
-    public Result players(){
-        return ok(views.html.admin.players.render());
-    }
-
-    public Result cups(){
-        return ok(views.html.admin.cups.render());
-    }
+//    public Result newCategory() {
+//        return ok(views.html.admin.categories_form.apply(Form.form(Category.class)));
+//    }
+//
+//    public Result tournaments(){
+//        return ok(views.html.admin.tournaments.render());
+//    }
+//
+//    public Result teams(){
+//        return ok(views.html.admin.teams.render());
+//    }
+//
+//    public Result players(){
+//        return ok(views.html.admin.players.render());
+//    }
+//
+//    public Result cups(){
+//        return ok(views.html.admin.cups.render());
+//    }
 }

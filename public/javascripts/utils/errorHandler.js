8d8115556e429556b3f5972ea3280293d3fc8235
Nicolas Schejtman <nschejtman93@gmail.com>
2016-01-24 19:45:40 +0100

    Refactor on errorHandler

--- public/javascripts/utils/errorHandler.js
+++ public/javascripts/utils/errorHandler.js
@@ -1,6 +1,6 @@
-function UIErrorHandler ($form) {
+function UIErrorHandler($form) {
 
-    this.displayError = function(field, error) {
+    function displayError(field, error) {
         var $formInput = $form.find('input[name=' + field + ']');
         var $formGroup = $formInput.parent('.form-group');
         var $crossIcon = $formGroup.find('.form-control-feedback');
@@ -8,31 +8,30 @@ function UIErrorHandler ($form) {
         $formGroup.toggleClass("has-feedback", true);
         $crossIcon.toggleClass("hidden", false);
         $formGroup.append($('<span class="help-block">' + error + '</span>'));
-    };
+    }
 
-    this.clearErrors = function(field){
+    function clearErrors(field) {
         var $formInput = $form.find('input[name=' + field + ']');
         var $formGroup = $formInput.parent('.form-group');
         var $crossIcon = $formGroup.find('.form-control-feedback');
         $formGroup.toggleClass("has-error", false);
         $formGroup.toggleClass("has-feedback", false);
         $crossIcon.toggleClass("hidden", true);
-        $formGroup.find('.help-block').each(function(){this.remove()});
-    };
+        $formGroup.find('.help-block').each(function () {
+            this.remove()
+        });
+    }
 
-    this.clearAllErrors = function(){
+    function clearAllErrors() {
         var $inputs = $form.find('input');
-        $.each($inputs, function(pos, input){
-            var $formInput = $form.find('input[name=' + $(input).attr("name") + ']');
-            var $formGroup = $formInput.parent('.form-group');
-            var $crossIcon = $formGroup.find('.form-control-feedback');
-            $formGroup.toggleClass("has-error", false);
-            $formGroup.toggleClass("has-feedback", false);
-            $crossIcon.toggleClass("hidden", true);
-            $formGroup.find('.help-block').each(function(){this.remove()});
-            //this.$rootScope.clearErrors($(input).attr('name'));
+        $.each($inputs, function (pos, input) {
+            clearErrors($(input).attr('name'));
         });
 
     }
+
+    this.displayError = displayError;
+    this.clearErrors = clearErrors;
+    this.clearAllErrors = clearAllErrors;
 
 }
